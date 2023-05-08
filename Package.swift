966225562b61e6b9e7888012af54d507c451aa89
Rene Cacheaux <rene.cacheaux@gmail.com>
2023-05-08 14:39:57 -0500

    Updates package name.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BulletinRemoteAPI",
+    name: "bulletin-remote-api-client",
     defaultLocalization: "en",
     platforms: [
         .iOS("16.4")
